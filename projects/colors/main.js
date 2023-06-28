6e81e5a74740e61d9fb704bc357a089493b9604b
Seungrok <sonicrok93@gmail.com>
2023-06-29 00:51:19 +0900

    feat: Change background color when a button is clicked

---
+++ projects/colors/main.js
@@ -0,0 +1,13 @@
+import { colorTable } from './colorTable.js';
+
+const button = document.querySelector('button');
+
+let index = 0;
+
+function getNextColor(e) {
+  const keys = Object.keys(colorTable);
+  e.target.parentNode.style.backgroundColor = colorTable[keys[index++]];
+  index %= keys.length;
+}
+
+button.addEventListener('click', getNextColor);
