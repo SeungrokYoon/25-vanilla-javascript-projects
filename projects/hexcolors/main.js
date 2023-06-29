19713176eb02da329eeaab7107e071a90edfbb86
Seungrok <sonicrok93@gmail.com>
2023-06-30 00:31:52 +0900

    feat: Add initial markup, styles, and basic element selector scripts

---
+++ projects/hexcolors/main.js
@@ -0,0 +1,5 @@
+const wrapper = document.querySelector('.wrapper');
+const hexText = document.querySelector('.hex-text');
+const btnContainer = document.querySelector('.btn-container');
+const generateBtn = document.querySelector('#generate');
+const copyBtn = document.querySelector('#copy');
