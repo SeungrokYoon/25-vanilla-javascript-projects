ad8adacbcb983a81036713af1a50df70f83a3d66
Seungrok <sonicrok93@gmail.com>
2023-07-02 23:38:29 +0900

    feat: Generate random  hexcolor code

--- projects/hexcolors/main.js
+++ projects/hexcolors/main.js
@@ -1,5 +1,14 @@
-const wrapper = document.querySelector('.wrapper');
+const background = document.querySelector('.background');
 const hexText = document.querySelector('.hex-text');
 const btnContainer = document.querySelector('.btn-container');
-const generateBtn = document.querySelector('#generate');
-const copyBtn = document.querySelector('#copy');
+const generateBtn = document.querySelector('#generate-btn');
+const copyBtn = document.querySelector('#copy-btn');
+
+function getRandomHexText() {
+  const randomHex =
+    '#' +
+    parseInt(Math.random() * 2 ** 16)
+      .toString(16)
+      .padStart(6, 0);
+  return randomHex;
+}
