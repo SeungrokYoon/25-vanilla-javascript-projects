07f1c91d96392ea80d7fea8bbcece8b1f4aa9e4d
Seungrok <sonicrok93@gmail.com>
2023-07-08 14:41:17 +0900

    feat: Fetch quote and update it to the screen

--- projects/random-quotes/main.js
+++ projects/random-quotes/main.js
@@ -1,5 +1,34 @@
+const quoteParagraph = document.querySelector('.quote');
+const authorSpan = document.querySelector('.author');
 const generateBtn = document.querySelector('.generate-btn');
 
-generateBtn.addEventListener('click', () => {
-  alert('click');
-});
+async function getQuotes(url = '') {
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Api-Key': config.api_ninjas_key,
+      },
+    });
+    if (!response.ok) {
+      throw new Error('네트워크 오류');
+    }
+    return response.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function handleClick() {
+  const [{ author, quote }] = await getQuotes(
+    'https://api.api-ninjas.com/v1/quotes'
+  );
+  const quoteElement = document.createElement('q');
+  quoteElement.innerText = quote;
+  quoteParagraph.innerHTML = '';
+  quoteParagraph.appendChild(quoteElement);
+  authorSpan.innerText = '- ' + author;
+}
+
+generateBtn.addEventListener('click', handleClick);
