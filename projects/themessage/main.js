e0dc92d7afc3adaf191d0302015b51fd056b5520
Seungrok <sonicrok93@gmail.com>
2023-07-21 12:26:43 +0900

    feat: Submit mesage on keydown 'ENTER'

--- projects/themessage/main.js
+++ projects/themessage/main.js
@@ -1,9 +1,19 @@
+const inputEl = document.getElementById('message-input');
 const submitBtn = document.getElementById('submit');
 const outputMessageEl = document.getElementById('output-message');
 
-submitBtn.addEventListener('click', () => {
-  const inputEl = document.getElementById('message-input');
-  const inputText = inputEl.value;
-  inputEl.value = '';
-  outputMessageEl.innerHTML = inputText;
+function submitMessage({ start, destination }) {
+  const inputText = start.value;
+  start.value = '';
+  destination.innerHTML = inputText;
+}
+
+submitBtn.addEventListener('click', (e) => {
+  submitMessage({ start: inputEl, destination: outputMessageEl });
+});
+
+inputEl.addEventListener('keydown', (e) => {
+  if (e.code === 'Enter') {
+    submitMessage({ start: e.target, destination: outputMessageEl });
+  }
 });
